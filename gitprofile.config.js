c36a342da24e904c9a597ed0625d27946407291f
Vigneshwaran J <vignsh0104@gmail.com>
2023-08-19 11:25:54 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -147,7 +147,7 @@ const config = {
   blog: {
     source: 'medium', // medium | dev
     username: 'vckie', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    limit: 10, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
